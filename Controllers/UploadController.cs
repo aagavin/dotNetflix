b9dff42d49c4ecb0e82d1c629cff4540597d14b2
aagavin <aagavin@gmail.com>
2017-10-03 23:00:05 -0400

    upload controller

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dotNetflix.Models;
+
+namespace dotNetflix.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
