a8ffc8cf86e3f53dec6a54f6890019c6f0c84d67
aagavin <aagavin@gmail.com>
2017-10-07 13:57:27 -0400

    formatting

--- Models/DbModel.cs
+++ Models/DbModel.cs
@@ -3,24 +3,27 @@ using System.Collections.Generic;
 
 namespace dotNetflix.Models
 {
-	public class Users{
+	public class Users
+	{
 
 		public int Userid { get; set; }
 		public string Username { get; set; }
-		public string Password{get; set;}
-		public virtual ICollection<Videos> Videos{get; set;}
-		public virtual ICollection<Comments> Comments{get; set;}
+		public string Password { get; set; }
+		public virtual ICollection<Videos> Videos { get; set; }
+		public virtual ICollection<Comments> Comments { get; set; }
 	}
 
-	public class Videos{
+	public class Videos
+	{
 		public int Videoid { get; set; }
 		public virtual Users User { get; set; }
 		public int views { get; set; }
 		public string bucketurl { get; set; }
-		public virtual ICollection<Comments> Comments{get; set;}
+		public virtual ICollection<Comments> Comments { get; set; }
 	}
 
-	public class Comments{
+	public class Comments
+	{
 		public int Videoid { get; set; }
 		public int Userid { get; set; }
 		public string Comment { get; set; }
