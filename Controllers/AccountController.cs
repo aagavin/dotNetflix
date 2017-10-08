caeae5d65e70f64c5bff6a4375715892e7cdd45a
aagavin <aagavin@gmail.com>
2017-10-08 13:25:15 -0400

    formatting

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -11,97 +11,99 @@ using Microsoft.AspNetCore.Authentication;
 
 namespace dotNetflix.Controllers
 {
-    public class AccountController : Controller
-    {
-      public IActionResult Index()
-      {
-        return RedirectToAction("Index", "Home");
-      }
+	public class AccountController : Controller
+	{
+		public IActionResult Index()
+		{
+			return RedirectToAction("Index", "Home");
+		}
 
-      public IActionResult Signin()
-      {
-        return View();
-      }
+		public IActionResult Signin()
+		{
+			return View();
+		}
 
-      [HttpPost]
-      public async Task<IActionResult> Signin([FromForm] string username, [FromForm] string password, string returnUrl = null)
-      {
-        Console.WriteLine($"{username} {password}");
+		[HttpPost]
+		public async Task<IActionResult> Signin([FromForm] string username, [FromForm] string password, string returnUrl = null)
+		{
+			Console.WriteLine($"{username} {password}");
 
-        if (username == null || password == null)
-        {
-          return View();
-        }
+			if (username == null || password == null)
+			{
+				return View();
+			}
 
-        using (var context = new DbSqlContext())
-        {
-          context.Database.EnsureCreated();
+			using (var context = new DbSqlContext())
+			{
+				context.Database.EnsureCreated();
 
-          Users user = context.Users.Where(u => u.Username == username).FirstOrDefault();
-          var claims = new List<Claim>{
-            new Claim("id", user.Userid.ToString()),
-            new Claim("name", user.Username),
-            new Claim("role", "user")
-          };
+				Users user = context.Users.Where(u => u.Username == username).FirstOrDefault();
+				var claims = new List<Claim>{
+						new Claim("id", user.Userid.ToString()),
+						new Claim("name", user.Username),
+						new Claim("role", "user")
+					};
 
-          var ci = new ClaimsIdentity(claims, "password", "name", "role");
-          var p = new ClaimsPrincipal(ci);
-          
-          await HttpContext.SignInAsync(p);
+				var ci = new ClaimsIdentity(claims, "password", "name", "role");
+				var p = new ClaimsPrincipal(ci);
+
+				await HttpContext.SignInAsync(p);
 
 
-        }
+			}
 
-        if(Url.IsLocalUrl(returnUrl)){
-          return Redirect(returnUrl);
-        }
-        else{
-          return Redirect("/");
-        }
-        return RedirectToAction("Index", "Home");
-      }
+			if (Url.IsLocalUrl(returnUrl))
+			{
+				return Redirect(returnUrl);
+			}
+			else
+			{
+				return Redirect("/");
+			}
+		}
 
-      public String Forbidden()
-      {
-        return "You are not allowed to access this pages";
-      }
+		public async Task<IActionResult> Logout()
+		{
+			await HttpContext.SignOutAsync();
+			return Redirect("/");
+		}
 
-      public IActionResult Create()
-      {
-        return View();
-      }
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-      [HttpPost]
-      public IActionResult Create([FromForm] string username, [FromForm] string password, [FromForm] string passwordverify)
-      {
-        Console.WriteLine($"{username} {password} {passwordverify} {password.Equals(passwordverify)}");
+		[HttpPost]
+		public IActionResult Create([FromForm] string username, [FromForm] string password, [FromForm] string passwordverify)
+		{
+			Console.WriteLine($"{username} {password} {passwordverify} {password.Equals(passwordverify)}");
 
-        if (username == null || password == null || passwordverify == null)
-        {
-          ViewData["Message"] = "Error all items need to be created";
-          return View();
-        }
-        else if (!password.Equals(passwordverify))
-        {
-          ViewData["Message"] = $"Passwords dont match {password} {passwordverify}";
-          return View();
-        }
+			if (username == null || password == null || passwordverify == null)
+			{
+				ViewData["Message"] = "Error all items need to be created";
+				return View();
+			}
+			else if (!password.Equals(passwordverify))
+			{
+				ViewData["Message"] = $"Passwords dont match {password} {passwordverify}";
+				return View();
+			}
 
-        PasswordHash passwordHash = new PasswordHash(password);
+			PasswordHash passwordHash = new PasswordHash(password);
 
-        using (var context = new DbSqlContext())
-        {
-          context.Database.EnsureCreated();
+			using (var context = new DbSqlContext())
+			{
+				context.Database.EnsureCreated();
 
-          Users user = new Users();
-          user.Username = username;
-          user.Password = System.Text.Encoding.UTF8.GetString(passwordHash.ToArray());
-          context.Users.Add(user);
-          context.SaveChanges();
-        }
-        ViewData["Message"] = "Account created";
-        return RedirectToAction("Index");
-      }
+				Users user = new Users();
+				user.Username = username;
+				user.Password = System.Text.Encoding.UTF8.GetString(passwordHash.ToArray());
+				context.Users.Add(user);
+				context.SaveChanges();
+			}
+			ViewData["Message"] = "Account created";
+			return RedirectToAction("Index");
+		}
 
-    }
+	}
 }
