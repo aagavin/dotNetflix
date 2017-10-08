ca66ab092ed95e1beac0b3f85f36e51d7fc49514
aagavin <aagavin@gmail.com>
2017-10-08 10:29:47 -0400

    start up cookie config

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotNetflix.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,49 +12,52 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace dotNetflix
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-            services.AddAuthentication("MyCookieAuthenticationScheme").AddCookie(options => {
-                options.AccessDeniedPath ="/account/Forbidden";
-                options.LoginPath = "/account/signin";
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+			services.AddAuthentication("Cookies")
+			.AddCookie("Cookies", options => {
+				options.LoginPath = "/account/signin";
+				options.AccessDeniedPath = "/account/signin";
+			});
 
-            });
+			// claims transformation
+			services.AddTransient<IClaimsTransformation, ClaimsTransformer>();
 
-        }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+		}
 
-            app.UseStaticFiles();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+			app.UseAuthentication();
+			app.UseStaticFiles();
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+									name: "default",
+									template: "{controller=Home}/{action=Index}/{id?}");
+			});
 
-            app.UseAuthentication();
-        }
-    }
+		}
+	}
 }
