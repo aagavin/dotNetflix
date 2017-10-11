7e3bc9a9e48f6fb0c460733026ef75e1dc68e276
aagavin <aagavin@gmail.com>
2017-10-10 23:33:28 -0400

    get file

--- Controllers/UploadController.cs
+++ Controllers/UploadController.cs
@@ -6,16 +6,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using dotNetflix.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace dotNetflix.Controllers
 {
+    [Authorize]
     public class UploadController : Controller
     {
-        [Authorize]
+        
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index([FromForm] IFormFile file){
+
+            // BucketAccess bucket = new BucketAccess();
+            System.Console.WriteLine("*****************");
+            System.Console.WriteLine(file.FileName);
+            System.Console.WriteLine(file.ContentType);
+            System.Console.WriteLine("*****************");
+            return RedirectToAction("Select");
+        }
+
+        public string Select(){
+            return "You are here";
+        }
+
 
     }
 }
