e3bf8db971f3f701574aa6f5dad2127dabb3d9d3
aagavin <aagavin@gmail.com>
2017-10-14 15:48:19 -0400

    user controller

--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using dotNetflix.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotNetflix.Controllers
 {
@@ -14,6 +15,19 @@ namespace dotNetflix.Controllers
     {
         public IActionResult Index()
         {
+            using(var context = new DbSqlContext()){
+                context.Database.EnsureCreated();
+
+                List<Videos> videos =context.Videos.ToList();
+                foreach (Videos video in videos)
+                {
+                    System.Console.WriteLine("**********************************");
+                    System.Console.WriteLine(video.User.Userid);
+                    System.Console.WriteLine(video.Name);
+                    System.Console.WriteLine(video.bucketurl);
+                    System.Console.WriteLine("**********************************");
+                }
+            }
             return View();
         }
 
