1f97ff742e498a8281b9a8c3dc6faeb2a14961f8
aagavin <aagavin@gmail.com>
2017-10-22 14:10:00 -0400

    edit and delete

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -8,109 +8,149 @@ using dotNetflix.Models;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 
 namespace dotNetflix.Controllers
 {
-    public class AccountController : Controller
-    {
-    public IActionResult Index()
-    {
-        return RedirectToAction("Index", "Home");
-    }
+	public class AccountController : Controller
+	{
+        [Authorize]
+		public IActionResult Index()
+		{
+			using (var context = new DbSqlContext())
+			{
+				context.Database.EnsureCreated();
+                // get userid of logged in user
+                // var user = context.Users.Find(int.Parse(this.User.Claims.FirstOrDefault().Value));
+                int userid =int.Parse(this.User.Claims.FirstOrDefault().Value);
+                var comments = context.Comments.Where(c => c.User.Userid == userid).ToList();
 
-    public IActionResult Signin()
-    {
-        return View();
-    }
+                ViewData["Comments"] = comments;
+                return View();
+			}
+			// return RedirectToAction("Index", "Home");
+		}
 
-    [HttpPost]
-    public async Task<IActionResult> Signin([FromForm] string username, [FromForm] string password, string returnUrl = null)
-    {
-        Console.WriteLine($"{username} {password}");
-
-        if (username == null || password == null)
+        [Authorize]
+        public IActionResult UpdateComment([FromForm] string userComment, [FromForm] int commentId)
         {
-        return View();
+            using(var context = new DbSqlContext()){
+                context.Database.EnsureCreated();
+
+                var comment = context.Comments.Find(commentId);
+                comment.UserComment = userComment;
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
         }
 
-        using (var context = new DbSqlContext())
-        {
-        context.Database.EnsureCreated();
-
-        User user = context.Users.Where(u => u.Username == username).FirstOrDefault();
-
-        PasswordHash passwordHash = new PasswordHash(user.Password);
-
-        if(!passwordHash.Verify(password)){
-            return Redirect("/account/signin");
+        [Authorize]
+        public IActionResult DeleteComment([FromBody] int commentId){
+            using(var context = new DbSqlContext()){
+                context.Database.EnsureCreated();
+                var comment = context.Comments.Find(commentId);
+                context.Remove(comment);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
 
-        var claims = new List<Claim>{
-            new Claim("id", user.Userid.ToString()),
-            new Claim("name", user.Username),
-            new Claim("role", "user")
-            };
+		public IActionResult Signin()
+		{
+			return View();
+		}
 
-        var ci = new ClaimsIdentity(claims, "password", "name", "role");
-        var p = new ClaimsPrincipal(ci);
+		[HttpPost]
+		public async Task<IActionResult> Signin([FromForm] string username, [FromForm] string password, string returnUrl = null)
+		{
+			Console.WriteLine($"{username} {password}");
 
-        await HttpContext.SignInAsync(p);
+			if (username == null || password == null)
+			{
+				return View();
+			}
+
+			using (var context = new DbSqlContext())
+			{
+				context.Database.EnsureCreated();
+
+				User user = context.Users.Where(u => u.Username == username).FirstOrDefault();
+
+				PasswordHash passwordHash = new PasswordHash(user.Password);
+
+				if (!passwordHash.Verify(password))
+				{
+					return Redirect("/account/signin");
+				}
+
+				var claims = new List<Claim>{
+						new Claim("id", user.Userid.ToString()),
+						new Claim("name", user.Username),
+						new Claim("role", "user")
+						};
+
+				var ci = new ClaimsIdentity(claims, "password", "name", "role");
+				var p = new ClaimsPrincipal(ci);
+
+				await HttpContext.SignInAsync(p);
 
 
-        }
+			}
 
-        if (Url.IsLocalUrl(returnUrl))
-        {
-        return Redirect(returnUrl);
-        }
-        else
-        {
-        return Redirect("/");
-        }
-    }
+			if (Url.IsLocalUrl(returnUrl))
+			{
+				return Redirect(returnUrl);
+			}
+			else
+			{
+				return Redirect("/");
+			}
+		}
 
-    public async Task<IActionResult> Logout()
-    {
-        await HttpContext.SignOutAsync();
-        return Redirect("/");
-    }
+        [Authorize]
+		public async Task<IActionResult> Logout()
+		{
+			await HttpContext.SignOutAsync();
+			return Redirect("/");
+		}
 
-    public IActionResult Create()
-    {
-        return View();
-    }
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-    [HttpPost]
-    public IActionResult Create([FromForm] string username, [FromForm] string password, [FromForm] string passwordverify)
-    {
-        Console.WriteLine($"{username} {password} {passwordverify} {password.Equals(passwordverify)}");
+		[HttpPost]
+		public IActionResult Create([FromForm] string username, [FromForm] string password, [FromForm] string passwordverify)
+		{
+			Console.WriteLine($"{username} {password} {passwordverify} {password.Equals(passwordverify)}");
 
-        if (username == null || password == null || passwordverify == null)
-        {
-        ViewData["Message"] = "Error all items need to be created";
-        return View();
-        }
-        else if (!password.Equals(passwordverify))
-        {
-        ViewData["Message"] = $"Passwords dont match {password} {passwordverify}";
-        return View();
-        }
+			if (username == null || password == null || passwordverify == null)
+			{
+				ViewData["Message"] = "Error all items need to be created";
+				return View();
+			}
+			else if (!password.Equals(passwordverify))
+			{
+				ViewData["Message"] = $"Passwords dont match {password} {passwordverify}";
+				return View();
+			}
 
-        PasswordHash passwordHash = new PasswordHash(password);
+			PasswordHash passwordHash = new PasswordHash(password);
 
-        using (var context = new DbSqlContext())
-        {
-        context.Database.EnsureCreated();
+			using (var context = new DbSqlContext())
+			{
+				context.Database.EnsureCreated();
 
-        User user = new User();
-        user.Username = username;
-        user.Password = passwordHash.ToArray();
-        context.Users.Add(user);
-        context.SaveChanges();
-        }
-        ViewData["Message"] = "Account created";
-        return RedirectToAction("Index");
-    }
+				User user = new User();
+				user.Username = username;
+				user.Password = passwordHash.ToArray();
+				context.Users.Add(user);
+				context.SaveChanges();
+			}
+			ViewData["Message"] = "Account created";
+			return RedirectToAction("Index");
+		}
 
-    }
+	}
 }
